0b295e2d82e408dfec086c7d0bfc524c7c4fd783
Anirudh <anirudhswaminathan20@gmail.com>
2025-02-06 11:26:35 +0530

    complete signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/signin' element={<SignIn />} />
+        <Route path='/sign-in' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/about' element={<About />} />
         <Route path='/profile' element={<Profile />} />
